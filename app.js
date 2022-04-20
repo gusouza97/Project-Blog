d07ee9d012d0c8afd948c7db438b14ac8e52eeb8
Gustavo Souza <36318691+gusouza97@users.noreply.github.com>
2022-04-20 14:24:36 -0300

    A41
    Implementado ate a aula 41

--- app.js
+++ app.js
@@ -5,11 +5,31 @@ const bodyParser = require('body-parser')
 const admin = require('./routes/admin')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // Definindo a variavel que vai receber o Express
 const app = express();
 
-// Configuracaoes
+// ---------------------------------- Configuracaoes ----------------------------------
+// Session
+// Middleware para criacao de uma sessao
+app.use(session({
+    secret: "test", // Importante que essa Secret seja segura
+    resave: true,
+    saveUninitialized: true
+}))
+//Configurando o flash
+app.use(flash())
+
+//Middleware
+app.use((req, res, next) => {
+    // Declarando variaveis globais
+    res.locals.success_msg = req.flash("success_msg")
+    res.locals.error_msg = req.flash("error_msg")
+    next();
+})
+
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -38,7 +58,8 @@ mongoose.connect("mongodb://localhost/blogapp").then(() => {
 // Informando o Express que todos os arquivos estaticos estao na pasta Public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// --- Rotas
+
+// ---------------------------------- Rotas ----------------------------------
 
 app.get('/', (req, res) => {
     res.send("Rota Principal")
@@ -51,7 +72,7 @@ app.get('/posts', (req, res) => {
 // Usando o arquivo de rotas externo - ADMIN
 app.use('/admin', admin)
 
-// Outros
+// ---------------------------------- Outros ----------------------------------
 const PORT = 8081
 app.listen(PORT, () => {
     console.log("Servidor Rodando!")
--- routes/admin.js
+++ routes/admin.js
@@ -15,23 +15,57 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias")
+    // Trazendo todas as informacoes da collection
+    Categoria.find().sort({date: 'desc'})
+    .then((categorias) => {
+        res.render("admin/categorias", {categorias: categorias})
+    })
+    .catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias")
+        res.redirect("/admin")
+    })
+    
 })
 
 router.post("/categorias/nova", (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    // Verificando o formulario enviado pelo usuario
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome invalido"})
     }
 
-    new Categoria(novaCategoria).save()
-    .then(() => {
-        console.log("Categoria cadastrada com sucesso!")
-    })
-    .catch((err) => {
-        console.log("Houve um erro ao registrar no banco de dados")
-    })
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug invalido"})
+    }
 
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria eh muito pequeno"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save()
+        .then(() => {
+            // Armazenando uma Flash Message de Sucesso
+            req.flash("success_msg", "Categoria criada com sucesso")
+            // Redirecionando o usuario para a pagina de lista de categorias
+            res.redirect("/admin/categorias")
+        })
+        .catch((err) => {
+            // Armazenando uma Flash Message de Erro
+            req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente")
+            // Redirecionando o usuario para a pagina de admin
+            res.redirect("/admin")
+        })
+    }
 })
 
 router.get('/categorias/add', (req, res) => {
