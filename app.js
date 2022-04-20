a371816e64c876d2e4bc2a6dcb4f6d30912de781
Gustavo Souza <36318691+gusouza97@users.noreply.github.com>
2022-04-20 11:46:32 -0300

    A36
    Implementacao da Aula 36

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const admin = require('./routes/admin')
 const path = require('path')
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 // Definindo a variavel que vai receber o Express
 const app = express();
@@ -27,7 +27,12 @@ app.set('view engine', 'handlebars');
 app.set('views', './views')
 
 // Mongoose
-
+mongoose.connect("mongodb://localhost/blogapp").then(() => {
+    console.log("Conectado ao banco de dados com sucesso!")
+})
+.catch((err) => {
+    console.log("Houve um erro em se conectar: " + err);
+})
 
 // Public
 // Informando o Express que todos os arquivos estaticos estao na pasta Public
