ad8e3648946f6ada2fe6012af278c11e706db8cc
Gustavo Souza <36318691+gusouza97@users.noreply.github.com>
2022-04-24 13:16:19 -0300

    A61
    Implementacao da aula 61

--- routes/usuario.js
+++ routes/usuario.js
@@ -100,4 +100,11 @@ router.post("/login", (req, res, next) => {
     })(req, res, next)
 })
 
+router.get("/logout", (req, res) => {
+    // funcao do passport para logout
+    req.logout()
+    req.flash("success_msg", "Deslogado")
+    res.redirect("/")
+})
+
 module.exports = router
